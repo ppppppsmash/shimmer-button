79470e70354ee4ef8126efb9984d1155f045f63c
kurosawa <pppppp.work@gmail.com>
2025-01-15 13:01:58 +0900

    fix: final product

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,16 @@ import './App.css';
 function App() {
 
   return (
-    <main className="min-h-svh flex justify-center items-center w-full">
+    <main className="min-h-screen flex justify-center items-center w-full">
       <button
         type="button"
-        className="
-        px-24 py-8 text-3xl font-bold bg-[conic-gradient(from_var(--shimmer-angle),theme(colors.slate.950)_0%,theme(colors.slate.500)_10%,theme(colors.slate.950)_20%)] animate-[shimmer_2.5s_linear_infinite] rounded-[24px]
-        relative
-        
-        after:flex after:absolute after:bg-slate-950 after:inset-[2px] after:rounded-[22px] after:content-[attr(aria-label)]
-        after:items-center after:justify-center
-        "
+        className="relative px-24 py-8 text-3xl font-bold
+          bg-[conic-gradient(from_var(--shimmer-angle),theme(colors.slate.950)_0%,theme(colors.slate.500)_10%,theme(colors.slate.950)_20%)] animate-[shimmer_2.5s_linear_infinite] rounded-[24px]
+          after:flex after:absolute after:bg-slate-950 after:inset-[2px] after:rounded-[22px] after:content-[attr(aria-label)]
+          after:items-center after:justify-center"
         aria-label="shimmer"
       >
-        <span className="opacity-0">SHIMMER</span>
+        SHIMMER
       </button>
     </main>
   )
